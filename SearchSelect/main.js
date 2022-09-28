bce1343b01226b5a45e2cc5786bf426839570af7
backlunp <backlunp@users.noreply.github.com>
2022-09-28 11:02:08 -0400

    rewriting event handlers

--- SearchSelect/main.js
+++ SearchSelect/main.js
@@ -1,33 +1,4 @@
 customElements.define(
-  "search-in-select",
-  class extends HTMLElement {
-    constructor() {
-      super();
-
-      const template = document.getElementById("search-in-select-template");
-      const templateContent = template.content;
-
-      this.attachShadow({ mode: "open" }).appendChild(
-        templateContent.cloneNode(true)
-      );
-    }
-
-    connectedCallback() {
-      this.shadowRoot
-        .getElementById("search-input")
-        .addEventListener("change", (e) => {
-          console.log(e.target.value);
-        });
-      console.log(this);
-    }
-
-    disconnectedCallback() {
-      this.shadowRoot.getElementById("search-input").removeEventListener();
-    }
-  }
-);
-
-customElements.define(
   "search-select",
   class extends HTMLElement {
     constructor() {
@@ -43,8 +14,9 @@ customElements.define(
         templateContent.cloneNode(true)
       );
 
-      this.searchInputGroupElement =
-        this.shadowRoot.getElementById("search-input-group");
+      this.searchInputGroupElement = this.shadowRoot.getElementById(
+        "search-input-group"
+      );
       this.searchInputElement = this.shadowRoot.getElementById("search-input");
       this.filteredItemsElement = this.shadowRoot.getElementById(
         "filtered-items-text"
@@ -57,33 +29,28 @@ customElements.define(
       this.options = this.querySelectorAll("option");
     }
 
+    initializeFilteredItems = () => {
+      const filteredItemDivs = [...this.options].map((opt) => {
+        let li = document.createElement("div");
+        li.type = "";
+        // li.form = this.searchInputGroupElement;
+        li.innerText = opt.innerHTML;
+        li.dataset.value = opt.value;
+        return li;
+      });
+
+      this.filteredItemsElement.replaceChildren(...filteredItemDivs);
+    };
+
     populateFilteredItems = (e) => {
       let filteredOpts = [
-        ...this.querySelectorAll("option:not([selected])"),
-      ].filter(
+        ...this.filteredItemsElement.querySelectorAll("div")
+      ].foreach(
         (opt) =>
           opt.innerHTML
             .toLowerCase()
             .search(this.searchInputElement.value.toLowerCase()) >= 0
       );
-
-      const filteredItemDivs = filteredOpts.map((opt) => {
-        let li = document.createElement("input");
-        li.type = "button";
-        // li.form = this.searchInputGroupElement;
-        li.value = opt.innerHTML;
-        li.dataset.value = opt.value;
-        li.addEventListener("click", (e) => {
-          opt.setAttribute("selected", "true");
-          // this.searchInputElement.dispatchEvent(new Event("change"));
-          this.populateSelectedItems();
-          this.populateFilteredItems();
-          this.dispatchEvent(new Event("change"));
-        });
-        return li;
-      });
-
-      this.filteredItemsElement.replaceChildren(...filteredItemDivs);
     };
 
     populateSelectedItems = (e) => {
@@ -95,37 +62,60 @@ customElements.define(
         let li = document.createElement("div");
         li.innerText = opt.innerHTML;
         li.dataset.value = opt.value;
-        li.addEventListener("click", (e) => {
-          opt.removeAttribute("selected");
-
-          this.populateSelectedItems();
-          this.populateFilteredItems();
-          this.dispatchEvent(new Event("change"));
-          // this.searchInputElement.dispatchEvent(new Event("change"));
-        });
         return li;
       });
       this.selectedItemsElement.replaceChildren(...selectedItemDivs);
       //this.dispatchEvent(new Event('change'));
     };
 
+    selectFilteredItem = (item) => {
+      //console.log("Select Filtered Item: ", item);
+      [...this.options]
+        .find((opt) => opt.value === item.dataset.value)
+        .setAttribute("selected", "");
+      this.populateSelectedItems();
+      this.populateFilteredItems();
+    };
+
+    removeSelectedItem = (item) => {
+      //console.log("Remove Selected Item: ", item);
+      [...this.options]
+        .find((opt) => opt.value === item.dataset.value)
+        .removeAttribute("selected");
+      this.populateSelectedItems();
+      this.populateFilteredItems();
+    };
+
+    bgChange = () => {
+      const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+      console.log("Random Color: ", rndCol);
+      return rndCol;
+    };
+
     connectedCallback() {
-      // this.filteredItemsElement.style.display = "none";
+      this.filteredItemsElement.style.display = "none";
+      this.initializeFilteredItems();
       this.populateFilteredItems();
       this.searchInputGroupElement.addEventListener("focusin", (e) => {
         this.filteredItemsElement.style.display = "block";
       });
-      this.searchInputGroupElement.addEventListener("focusout", (e) => {
-        //this.filteredItemsElement.style.display = "none";
-        console.log("focusout");
-      });
+      // this.searchInputGroupElement.addEventListener("focusout", (e) => {
+      //   //this.filteredItemsElement.style.display = "none";
+      //   console.log("focusout");
+      // });
       this.searchInputElement.addEventListener(
         "keyup",
         this.populateFilteredItems
       );
-      this.searchInputElement.addEventListener(
-        "click",
-        this.populateFilteredItems
+      // this.searchInputElement.addEventListener(
+      //   "click",
+      //   this.populateFilteredItems
+      // );
+      this.filteredItemsElement.addEventListener("click", (e) =>
+        this.selectFilteredItem(e.target)
+      );
+      this.selectedItemsElement.addEventListener("click", (e) =>
+        this.removeSelectedItem(e.target)
       );
       // this.searchInputElement.addEventListener(
       //   "change",
@@ -139,12 +129,17 @@ customElements.define(
     }
 
     disconnectedCallback() {
+      this.searchInputGroupElement.removeEventListener();
+      this.filteredItemsElement.removeEventListener();
       this.searchInputElement.removeEventListener();
       this.removeEventListener();
     }
   }
 );
 
-document.getElementById("search-select1").addEventListener("change", (e) => {
-  console.log("event received", e.target.selectedOptions);
-});
+// document.getElementById("search-select1").addEventListener("change", (e) => {
+//   console.log("event received", e.target.selectedOptions);
+// });
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
